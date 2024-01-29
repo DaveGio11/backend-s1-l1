2c775d889b68b7446a1eac1dfcd2009f46b1c093
DaveGio11 <145689622+DaveGio11@users.noreply.github.com>
2024-01-29 16:58:00 +0100

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Atleta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Atleta
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Categoria(int peso)
+        {
+            if (peso > 80)
+            {
+                return "peso massimo";
+            }
+            else
+            {
+                return "peso medio";
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dipendente dipendente = new Dipendente();
+            dipendente.Cognome = "Pinco";
+            dipendente.Nome = "Pallino";
+            dipendente.RuoloRicoperto = "Amministratore Delegato";
+            dipendente.Stipendio = 1560.52;
+            dipendente.DataOraEntrata = DateTime.Now;
+
+            Console.WriteLine(dipendente.ScriviDatiDipendente());
+            dipendente.FirmaEntrata();
+            dipendente.FirmaUscita();
+            
+
+            Atleta atleta1 = new Atleta();
+            atleta1.Name = "Gianni";
+            Console.WriteLine(atleta1.Name);
+            atleta1.Categoria(90);
+            Console.WriteLine(atleta1.Categoria(90));
+            Console.ReadLine();
+
+        }
+    }
+}
